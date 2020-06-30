c2533e66370bd05723a55abb0e2ae4fdc40f7495
Edward Guan <285006386@qq.com>
2020-06-30 08:29:22 +0800

    update

--- jobs/Deploy.groovy
+++ jobs/Deploy.groovy
@@ -1,34 +1,34 @@
 properties([
-        parameters(
-                [
-                        stringParam(
-                                name: 'GIT_REPO',
-                                defaultValue: ''
-                        ),
-                        stringParam(
-                                name: 'VERSION',
-                                defaultValue: ''
-                        ),
-                        choiceParam(
-                                name: 'ENV',
-                                choices: ['test', 'staging', 'production']
-                        )
-                ]
-        )
+    parameters(
+        [
+            stringParam(
+                name: 'GIT_REPO',
+                defaultValue: ''
+            ),
+            stringParam(
+                name: 'VERSION',
+                defaultValue: ''
+            ),
+            choiceParam(
+                name: 'ENV',
+                choices: ['test', 'staging', 'production']
+            )
+        ]
+    )
 ])
 
 pipeline {
 
     agent {
         kubernetes {
-            label 'deploy-service-pod'
+            label 'jenkins-agent'
             defaultContainer 'jnlp'
             yaml """
 apiVersion: v1
 kind: Pod
 metadata:
   labels:
-    job: deploy-service
+    job: habr-demo-deploy
 spec:
   containers:
   - name: git
@@ -50,16 +50,9 @@ spec:
                 container('git') {
                     script {
                         def revision = params.VERSION.substring(0, 7)
-                        withCredentials([[
-                                $class: 'UsernamePasswordMultiBinding',
-                                credentialsId: 'github',
-                                usernameVariable: 'USERNAME',
-                                passwordVariable: 'PASSWORD'
-                        ]]) {
-                            sh "git clone https://$USERNAME:$PASSWORD@github.com/gmandnepr/${params.GIT_REPO}.git"
-                            dir ("${params.GIT_REPO}") {
-                                sh "git checkout ${revision}"
-                            }
+                        sh "git clone https://github.com/guanwei/${params.GIT_REPO}.git"
+                        dir ("${params.GIT_REPO}") {
+                            sh "git checkout ${revision}"
                         }
                     }
                 }
@@ -71,8 +64,7 @@ spec:
                     script {
                         dir ("${params.GIT_REPO}") {
                             sh "./helm/setRevision.sh ${params.VERSION}"
-                            def registryIp = sh(script: 'getent hosts registry.kube-system | awk \'{ print $1 ; exit }\'', returnStdout: true).trim()
-                            sh "/helm upgrade ${params.ENV}-${params.GIT_REPO.toLowerCase()} helm/ --install --namespace ${params.ENV} --set registry=${registryIp}"
+                            sh "/helm upgrade ${params.ENV}-${params.GIT_REPO.toLowerCase()} helm/ --install --namespace ${params.ENV}"
                         }
                     }
                 }
